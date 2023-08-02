ca28465b938abace9a3f93e163f9120efeffe8a2
mastra90 <lukejmastrocola@gmail.com>
2023-08-02 22:31:17 +1000

    delete pop up styled and notes now take up entire page on desktop

--- src/Components/NoItems.jsx
+++ src/Components/NoItems.jsx
@@ -6,13 +6,13 @@ export default function NoLists() {
     return (
         <div className="no-lists-body"> 
             <p className="no-lists-label">
-                You have no Items!
+                Your List is empty.
             </p> 
             <p className="no-list-instructions">
-                To start your list, add an Item 
+                To start your list,  
             </p> 
             <p className="no-list-instructions2">
-            in the "Add Item" field above
+            add an Item in the "Add Item" field above
             </p>
             <p className="no-list-icon">
             <FontAwesomeIcon icon={faList} /> 
