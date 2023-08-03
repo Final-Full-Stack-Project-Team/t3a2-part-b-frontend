f2186f3062c57c2292119e108fee31c199102676
R-Morton <remann12@gmail.com>
2023-08-04 09:01:50 +1000

    selecting groups in list sharing page started

--- src/Components/ShareList.jsx
+++ src/Components/ShareList.jsx
@@ -9,7 +9,8 @@ export default function ShareList() {
     const cookie = `Bearer ${cookies.authorization}`
 
     const [groups, setGroups] = useState([])
-    const [checkedGroups, setCheckedGroups] = useState({})
+    const [checkedGroups, setCheckedGroups] = useState(new Set())
+    const [selectedMembers, setSelectedMembers] = useState([])
 
     useEffect(() => {
         findAllGroups(cookie)
@@ -18,28 +19,47 @@ export default function ShareList() {
         })
     }, [])
 
-    function selectGroup(group) {
-        setCheckedGroups(prevCheckedItems => ({
-          ...prevCheckedItems,
-          [group._id]: !prevCheckedItems[group._id]
-        }));
-        console.log(groups)
-      }
+    const handleCheckboxChange = (groupId) => {
+        setCheckedGroups((prevSelected) => {
+          const newSelected = new Set(prevSelected);
+          if (newSelected.has(groupId)) {
+            newSelected.delete(groupId); // Uncheck the checkbox
+          } else {
+            newSelected.add(groupId); // Check the checkbox
+          }
+          return newSelected;
+        });
+      };
 
-    function handleSubmit() {
-        const selectedGroupsUsers = groups.filter(group => checkedGroups)
-    }
+    // Handle form submission
+    const handleSubmit = () => {
+        const selectedMembersArray = new Set();
+        checkedGroups.forEach((groupId) => {
+        const group = groups.find((group) => group._id === groupId);
+        if (group) {
+            group.shared_with.forEach((member) => {
+                selectedMembersArray.add(member._id)
+            })
+        }
+        });
+        setSelectedMembers(Array.from(selectedMembersArray));
+    };
+
+    useEffect(() => {
+        console.log(selectedMembers);
+      }, [selectedMembers]);
 
     return(
         <div style={{color: "white"}}>
             {groups && groups.map((group) => {
                 return(
                     <div style={{ display: "flex", flexDirection: "row" }} key={group._id}>
-                        <input type="checkbox" onChange={() => selectGroup(group)} checked={checkedGroups} />
+                        <input type="checkbox" onChange={() => handleCheckboxChange(group._id)} checked={checkedGroups.has(group._id)} />
                         <p style={{color: checkedGroups[group._id] ? "green": "white"}}>{group.group_name}</p>
                     </div>
                 )
             })}
+            <button onClick={handleSubmit}>SUBMIT</button>
         </div>
     )
 }
