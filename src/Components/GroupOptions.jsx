269557629906ac05f2f7a2d4e2c1724a150cc9a2
mastra90 <lukejmastrocola@gmail.com>
2023-08-04 18:49:39 +1000

    overlay box now suits for either guest and admin

--- src/Components/GroupOptions.jsx
+++ src/Components/GroupOptions.jsx
@@ -1,20 +1,27 @@
 
 
 function GroupOptions(props) {
-    return (
-      <div className="group-overlay">
-        <div className="group-overlay-content">
-          <p className="group-overlay-text" onClick={props.handleLeaveGroup}>
-            Leave Group
+  const optionCount = props.isAdmin ? 2 : 1; // If admin, show 2 options; otherwise, show 1 option
+  const overlayHeight = 0.7 + 2.1 * optionCount + 'em'; // Adjust as needed based on your styling
+
+  const overlayStyle = {
+    height: overlayHeight,
+  };
+
+  return (
+    <div className="group-overlay" style={overlayStyle}>
+      <div className="group-overlay-content">
+        <p className="group-overlay-text" onClick={props.handleLeaveGroup}>
+          Leave Group
+        </p>
+        {props.isAdmin && (
+          <p className="group-overlay-text" onClick={props.handleDeleteGroup}>
+            Delete Group
           </p>
-          {props.isAdmin && (
-            <p className="group-overlay-text" onClick={props.handleDeleteGroup}>
-              Delete Group
-            </p>
-          )}
-        </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
+}
   
   export default GroupOptions;
