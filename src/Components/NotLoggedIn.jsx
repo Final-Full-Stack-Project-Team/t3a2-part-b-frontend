329ac930de58fffb2f909708b0eadef1e93f9e85
mastra90 <lukejmastrocola@gmail.com>
2023-08-06 21:01:28 +1000

    added link to sign in page

--- src/Components/NotLoggedIn.jsx
+++ src/Components/NotLoggedIn.jsx
@@ -1,18 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faListCheck} from '@fortawesome/free-solid-svg-icons'
 import "../Styles/not-logged-in.css";
+import { Link } from 'react-router-dom';
 
 export default function NotLoggedIn() {
     return (
         <div className="not-logged-in-body"> 
-            <p className="welcome-label">
-                Welcome to MinimaList!.
+            <p className="not-logged-in-welcome-label">
+                Welcome to MinimaList!
             </p> 
             <p className="please-login-label">
                 Please login to make a list.
             </p> 
             <p className="not-logged-in-instructions-label">
-                To start, click "Login" in the menu.
+                You can login by tapping <Link to='sign-in' className='login-here-link'>here</Link>
             </p>
            
             <p className="completed-icon">
